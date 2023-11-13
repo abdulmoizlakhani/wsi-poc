113fa57f731da59e5be298483d945a4e4f261cee
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2023-11-13 18:49:08 +0500

    Side drawer tests

--- wsi-poc-components/src/components/__tests__/SideDrawer.spec.js
+++ wsi-poc-components/src/components/__tests__/SideDrawer.spec.js
@@ -4,13 +4,17 @@ import SideDrawer from "../SideDrawer.vue";
 
 describe("SideDrawer.vue", () => {
   const productArray = Object.values(productList).filter(
-    (item, i) => typeof item === "object"
+    (item) => typeof item === "object"
   );
 
   const propsData = {
     isDrawerOpen: true,
     cartItems: [productArray[0], productArray[2], productArray[3]],
-    cartSubtotal: 0
+    cartSubtotal: 0,
+  };
+
+  const listeners = {
+    closeDrawer: jest.fn(),
   };
 
   const containerClasses = "bg-black w-full h-full absolute top-0 opacity-60";
@@ -18,6 +22,7 @@ describe("SideDrawer.vue", () => {
   it("renders SideDrawer correctly", () => {
     const wrapper = shallowMount(SideDrawer, {
       propsData,
+      listeners,
       data: function () {
         return { containerClasses };
       },
@@ -27,16 +32,16 @@ describe("SideDrawer.vue", () => {
     );
   });
 
-  // it('calls store action "closeDrawer" when "Drawer Overlay" is clicked', () => {
-  //   const wrapper = shallowMount(SideDrawer, {
-  //     store,
-  //     localVue,
-  //     data: function () {
-  //       return { containerClasses };
-  //     },
-  //   });
-  //   const overlay = wrapper.find('[data-test="side-drawer-overlay"]');
-  //   overlay.trigger("click");
-  //   expect(modules.drawer.actions.closeDrawer).toHaveBeenCalled();
-  // });
+  it('calls store action "closeDrawer" when "Drawer Overlay" is clicked', () => {
+    const wrapper = shallowMount(SideDrawer, {
+      propsData,
+      listeners,
+      data: function () {
+        return { containerClasses };
+      },
+    });
+    const overlay = wrapper.find('[data-test="side-drawer-overlay"]');
+    overlay.trigger("click");
+    expect(listeners.closeDrawer).toHaveBeenCalled();
+  });
 });
