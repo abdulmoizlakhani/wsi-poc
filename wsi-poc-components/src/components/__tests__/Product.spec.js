6ce330cf3b9b988d0e27a0db236732efeb30c303
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2023-11-13 18:37:39 +0500

    Product component unit tests

--- wsi-poc-components/src/components/__tests__/Product.spec.js
+++ wsi-poc-components/src/components/__tests__/Product.spec.js
@@ -8,36 +8,62 @@ describe("Product.vue", () => {
   );
 
   const propsData = {
-    product: productArray[1],
+    product: productArray[0],
     cartItems: [productArray[0], productArray[2], productArray[3]],
+  };
+
+  const listeners = {
+    onClickProductTile: jest.fn(),
+    addItemToCart: jest.fn(),
+    removeItemFromCart: jest.fn(),
+    onClickProductTile: jest.fn(),
   };
 
   it("renders product correctly", () => {
     const wrapper = shallowMount(Product, {
       propsData,
-      listeners: {
-        onClickProductTile: jest.fn(),
-        addItemToCart: jest.fn(),
-        removeItemFromCart: jest.fn(),
-        onClickProductTile: jest.fn(),
-      }
+      listeners,
+      computed: {
+        isSelected: jest.fn().mockReturnValue(true),
+      },
     });
     expect(wrapper.find('[data-test="product-container"]').exists()).toBe(true);
   });
 
-  // it('calls store action "addItemToCart" when "Add to Cart" button is clicked', () => {
-  //   const wrapper = shallowMount(Product, { propsData });
-  //   const button = wrapper.find("button");
-  //   button.trigger("click");
-  //   expect(modules.cart.actions.addItemToCart).toHaveBeenCalled();
-  // });
+  it("renders product correctly", () => {
+    const wrapper = shallowMount(Product, {
+      propsData: { ...propsData, cartItems: [] },
+      listeners,
+      computed: {
+        isSelected: jest.fn().mockReturnValue(false),
+      },
+    });
+    expect(wrapper.find('[data-test="product-container"]').exists()).toBe(true);
+  });
 
-  // it('calls store action "removeItemFromCart" when "Remove from Cart" button is clicked', () => {
-  //   const wrapper = shallowMount(Product, {
-  //     propsData: { product: productArray[2] },
-  //   });
-  //   const button = wrapper.find("button");
-  //   button.trigger("click");
-  //   expect(modules.cart.actions.removeItemFromCart).toHaveBeenCalled();
-  // });
+  it('calls store action "removeItemFromCart" when "Remove from Cart" button is clicked', () => {
+    const wrapper = shallowMount(Product, {
+      propsData,
+      listeners,
+      computed: {
+        isSelected: jest.fn().mockReturnValue(true),
+      },
+    });
+    const button = wrapper.find("button");
+    button.trigger("click");
+    expect(listeners.removeItemFromCart).toHaveBeenCalled();
+  });
+
+  it('calls store action "addItemToCart" when "Add to Cart" button is clicked', () => {
+    const wrapper = shallowMount(Product, {
+      propsData: { ...propsData, cartItems: [] },
+      listeners,
+      computed: {
+        isSelected: jest.fn().mockReturnValue(false),
+      },
+    });
+    const button = wrapper.find("button");
+    button.trigger("click");
+    expect(listeners.addItemToCart).toHaveBeenCalled();
+  });
 });
