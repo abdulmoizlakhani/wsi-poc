3bdfad9864b5858ec297bfb5f44305a62e4143b9
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2025-01-06 19:36:22 +0500

    Added chatbot

--- wsi-poc-app/src/App.vue
+++ wsi-poc-app/src/App.vue
@@ -41,6 +41,36 @@ export default {
       cart: require("./assets/grocery-store.png"),
     };
   },
+  mounted() {
+    if (!window.chatbase || window.chatbase("getState") !== "initialized") {
+      window.chatbase = (...arg) => {
+        if (!window.chatbase.q) {
+          window.chatbase.q = [];
+        }
+        window.chatbase.q.push(arg);
+      };
+      window.chatbase = new Proxy(window.chatbase, {
+        get(target, prop) {
+          if (prop === "q") {
+            return target.q;
+          }
+          return (...args) => target(prop, ...args);
+        },
+      });
+    }
+    const onLoad = function () {
+      const script = document.createElement("script");
+      script.src = "https://www.chatbase.co/embed.min.js";
+      script.id = process.env.VUE_APP_CHATBOT_ID;
+      script.domain = "www.chatbase.co";
+      document.body.appendChild(script);
+    };
+    if (document.readyState === "complete") {
+      onLoad();
+    } else {
+      window.addEventListener("load", onLoad);
+    }
+  },
   computed: {
     ...mapGetters("cart", ["cartItems", "cartSubtotal"]),
     ...mapGetters("drawer", ["isDrawerOpen"]),
